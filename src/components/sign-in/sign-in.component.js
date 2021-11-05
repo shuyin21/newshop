8a3d963f4f764918de263f363dd34cd584408ca9
shuyin21 <shuyindev@gmail.com>
2021-11-05 14:34:24 +0000

    custom button component created

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 const SignIn = () => {
 
@@ -50,7 +51,7 @@ const SignIn = () => {
                     required />
 
 
-                <input type='submit' value='Submit form' />
+                <CustomButton type='submit'>Sign in</CustomButton>
             </form>
 
 
