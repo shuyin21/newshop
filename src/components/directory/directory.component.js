d8483064c0d7e145d97e4d9b92726b6f28e6cdab
shuyin21 <shuyindev@gmail.com>
2021-11-05 06:30:41 +0000

    updated our app with shop page and collection item and preview component

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
+import hats from '../../images/hats.png';
+import jackets from '../../images/jackets.png';
+import sneakers from '../../images/sneakers.png';
+import womens from '../../images/womens.png';
+import men from '../../images/men.png';
+
 
 const Directory = () => {
 
@@ -8,32 +14,32 @@ const Directory = () => {
         sections: [
             {
                 title: 'hats',
-                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                imageUrl: `${hats}`,
                 id: 1,
                 linkUrl: 'hats'
             },
             {
                 title: 'jackets',
-                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                imageUrl: `${jackets}`,
                 id: 2,
                 linkUrl: ''
             },
             {
                 title: 'sneakers',
-                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                imageUrl: `${sneakers}`,
                 id: 3,
                 linkUrl: ''
             },
             {
                 title: 'womens',
-                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                imageUrl: `${womens}`,
                 size: 'large',
                 id: 4,
                 linkUrl: ''
             },
             {
                 title: 'mens',
-                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                imageUrl: `${men}`,
                 size: 'large',
                 id: 5,
                 linkUrl: ''
