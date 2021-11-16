311282c8b3bb17cfc0f34971e4b9515963078d50
shuyin21 <shuyindev@gmail.com>
2021-11-16 12:49:11 +0000

    created add Item and remove item functionality

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -10,7 +10,15 @@ export const addItem = item => ({
 });
 
 
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+})
+
 export const clearItemFromCart = item => ({
     type: CartActionTypes.CLEAR_ITEM_FORM_CART,
     payload: item
-})
+});
+
+
+
