cd8aae1b5a3852dab71bd542d910224b115fe891
shuyin21 <shuyindev@gmail.com>
2021-11-18 11:28:02 +0000

    stripe checkout button created

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -4,6 +4,9 @@ import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
+
 import {
     selectCartItems,
     selectCartTotal
@@ -33,7 +36,15 @@ const CheckoutPage = ({ cartItems, total }) => (
         {cartItems.map(cartItem => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
-        <div className='total'>TOTAL: £{total}</div>
+        <div className='total'>TOTAL:£{total}</div>
+
+        <div className='test-warning'>
+            *Please use the following test credit card for payment
+            <br />
+            4242 4242 4242 4242 - Exp: 01/22 - CVC: 123
+        </div>
+
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
