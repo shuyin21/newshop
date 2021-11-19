d456f88215fecd8703ed4d5a970339414d049fab
shuyin21 <shuyindev@gmail.com>
2021-11-19 18:00:45 +0000

    HeaderContainer component updated to styled component

---
+++ src/components/header/header.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+import { Link } from 'react-router-dom';
+
+
+const HeaderContainer = styled.div`
+height: 70px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 25px;
+
+`;
+
