6907dbc298912a0cc54405cba6c87287bdc34b0c
shuyin21 <shuyindev@gmail.com>
2021-11-23 07:20:25 +0000

    created collection convert

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -56,7 +56,19 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     await batch.commit()
 }
 
+export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
 
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+    console.log(transformedCollection);
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
