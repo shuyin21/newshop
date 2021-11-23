c1ae5a970794347142c67653c1d7e5c73702b98f
shuyin21 <shuyindev@gmail.com>
2021-11-23 06:59:17 +0000

    data uploaded to firebase, shop component changed to class based

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -36,6 +38,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+
     });
   }
 
@@ -70,6 +73,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
+
 });
 
 const mapDispatchToProps = dispatch => ({
