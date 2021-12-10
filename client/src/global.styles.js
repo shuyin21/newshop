5964d835e3347511c548a1e46ddc4f3b1fd5086b
shuyin21 <shuyindev@gmail.com>
2021-12-10 15:29:06 +0000

    global styles created

---
+++ client/src/global.styles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const globalStyle = createGlobalStyle`
+  body{
+    font-family: 'Open Sans Condensed';
+    padding: 20px 60px;
+}
+
+
+a{
+    text-decoration: none;
+    color:#000;
+}
+
+
+*{
+    box-sizing: border-box;
+}
+  `
