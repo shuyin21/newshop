5162ffde6e91052d1c31b5c681e61831e00e591c
shuyin21 <shuyindev@gmail.com>
2021-12-10 15:32:30 +0000

    global styles added to app.js

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const globalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   body{
     font-family: 'Open Sans Condensed';
     padding: 20px 60px;
